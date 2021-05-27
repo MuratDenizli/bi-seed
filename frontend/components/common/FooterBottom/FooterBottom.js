b9b7e3d8817963fe45fcc6c1cbd90494ffd4fc3d
Murat Denizli <extmuratde@d-teknoloji.com.tr>
2021-05-27 09:47:34 +0300

    Anasayfa tasarımı tamamlandı

--- frontend/components/common/FooterBottom/FooterBottom.js
+++ frontend/components/common/FooterBottom/FooterBottom.js
@@ -4,20 +4,140 @@ import styles from './FooterBottom.module.css'
 
 function FooterBottom() {
   return (
-    <div className={styles.footerBottom}>
-      <div className={styles.paymentlogos}>
-        <p>Güvenli Ödeme Sistemi Sunuyoruz</p>
-        <div>
-          <Image
-            src="/images/gomlek-kampanya-1.webp"
-            alt="Picture of the author"
-            layout="responsive"
-            width={100}
-            height={25}
-          />
+    <div className={styles.footerbottom}>
+      <div className={styles.logosarea}>
+        <div className={styles.paymentlogos}>
+          <p>Güvenli Ödeme Sistemi Sunuyoruz</p>
+          <div>
+            <div
+              style={{ position: 'relative', width: '80px', height: '20px' }}
+            >
+              <Image
+                src="/images/payment/3dsecure.png"
+                alt="Picture of the author"
+                layout="fill"
+                objectFit="contain"
+                quality={100}
+              />
+            </div>
+            <div
+              style={{ position: 'relative', width: '80px', height: '20px' }}
+            >
+              <Image
+                src="/images/payment/visa.png"
+                alt="Picture of the author"
+                layout="fill"
+                objectFit="contain"
+                quality={100}
+              />
+            </div>
+            <div
+              style={{ position: 'relative', width: '80px', height: '20px' }}
+            >
+              <Image
+                src="/images/payment/masterpass.png"
+                alt="Picture of the author"
+                layout="fill"
+                objectFit="contain"
+                quality={100}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles.cardlogos}>
+          <p>LC Waikiki'de Taksit Avantajları</p>
+          <div>
+            <div
+              style={{ position: 'relative', width: '80px', height: '20px' }}
+            >
+              <Image
+                src="/images/payment/advantage.png"
+                alt="Picture of the author"
+                layout="fill"
+                objectFit="contain"
+                quality={100}
+              />
+            </div>
+            <div
+              style={{ position: 'relative', width: '80px', height: '20px' }}
+            >
+              <Image
+                src="/images/payment/axess.png"
+                alt="Picture of the author"
+                layout="fill"
+                objectFit="contain"
+                quality={100}
+              />
+            </div>
+            <div
+              style={{ position: 'relative', width: '80px', height: '20px' }}
+            >
+              <Image
+                src="/images/payment/bankkartcombo.png"
+                alt="Picture of the author"
+                layout="fill"
+                objectFit="contain"
+                quality={100}
+              />
+            </div>
+            <div
+              style={{ position: 'relative', width: '80px', height: '20px' }}
+            >
+              <Image
+                src="/images/payment/bankkartcombo.png"
+                alt="Picture of the author"
+                layout="fill"
+                objectFit="contain"
+                quality={100}
+              />
+            </div>
+            <div
+              style={{ position: 'relative', width: '80px', height: '20px' }}
+            >
+              <Image
+                src="/images/payment/bonus.png"
+                alt="Picture of the author"
+                layout="fill"
+                objectFit="contain"
+                quality={100}
+              />
+            </div>
+            <div
+              style={{ position: 'relative', width: '80px', height: '20px' }}
+            >
+              <Image
+                src="/images/payment/maximum.png"
+                alt="Picture of the author"
+                layout="fill"
+                objectFit="contain"
+                quality={100}
+              />
+            </div>
+            <div
+              style={{ position: 'relative', width: '80px', height: '20px' }}
+            >
+              <Image
+                src="/images/payment/paraf.png"
+                alt="Picture of the author"
+                layout="fill"
+                objectFit="contain"
+                quality={100}
+              />
+            </div>
+            <div
+              style={{ position: 'relative', width: '80px', height: '20px' }}
+            >
+              <Image
+                src="/images/payment/world.png"
+                alt="Picture of the author"
+                layout="fill"
+                objectFit="contain"
+                quality={100}
+              />
+            </div>
+          </div>
         </div>
       </div>
-      <div className={styles.cardlogos}> </div>
     </div>
   )
 }
