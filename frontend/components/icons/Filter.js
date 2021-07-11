c04c58bb6bb70f77c4814766342a6bcceecc3cc3
Murat Denizli <muratdenizli0@gmail.com>
2021-07-11 04:17:06 +0300

    Create Filter.js

---
+++ frontend/components/icons/Filter.js
@@ -0,0 +1,24 @@
+import * as React from 'react'
+
+function SvgFilter(props) {
+  return (
+    <svg
+      aria-hidden="true"
+      data-prefix="fal"
+      data-icon="sort-amount-up-alt"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+      className="filter_svg__svg-inline--fa filter_svg__fa-sort-amount-up-alt filter_svg__fa-w-16 filter_svg__fa-9x"
+      width="1em"
+      height="1em"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M264 224h112a8 8 0 008-8v-16a8 8 0 00-8-8H264a8 8 0 00-8 8v16a8 8 0 008 8zm0 96h176a8 8 0 008-8v-16a8 8 0 00-8-8H264a8 8 0 00-8 8v16a8 8 0 008 8zm0-192h48a8 8 0 008-8v-16a8 8 0 00-8-8h-48a8 8 0 00-8 8v16a8 8 0 008 8zm240 256H264a8 8 0 00-8 8v16a8 8 0 008 8h240a8 8 0 008-8v-16a8 8 0 00-8-8zM120.44 35.51a11.94 11.94 0 00-16.87 0l-84 82.32a12 12 0 00-.09 17l5.61 5.68a11.93 11.93 0 0016.91.09l54-52.74V472a8 8 0 008 8h16a8 8 0 008-8V88.08l53.94 52.35a12 12 0 0016.92 0l5.64-5.66a12 12 0 000-17z"
+      />
+    </svg>
+  )
+}
+
+export default SvgFilter
